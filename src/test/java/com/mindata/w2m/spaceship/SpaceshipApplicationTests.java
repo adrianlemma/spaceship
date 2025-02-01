76a611afd946d56183f8cb14e75c44916655339a
unknown <adrian27lemma@gmail.com>
2025-02-01 15:53:31 -0300

    initialize project

---
+++ src/test/java/com/mindata/w2m/spaceship/SpaceshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindata.w2m.spaceship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
