cfd63768b11a407153af3f82c5dcb1af778d788f
unknown <adrian27lemma@gmail.com>
2025-02-04 05:27:41 -0300

    Add Swagger configurations

--- src/main/java/com/mindata/w2m/spaceship/configuration/SecurityConfiguration.java
+++ src/main/java/com/mindata/w2m/spaceship/configuration/SecurityConfiguration.java
@@ -41,6 +41,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.GET, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/**").hasRole(userRole)
                         .requestMatchers(HttpMethod.PUT, "/**").hasRole(userRole)
